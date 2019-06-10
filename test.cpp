c9040744f8ab4a651d54d425bdf7b1de251fd4f9
Raphael Zulliger <zulliger@software-natives.ch>
2019-06-10 08:35:58 +0200

    Adding some text containing 'the'

--- test.cpp
+++ test.cpp
@@ -4,5 +4,6 @@ int main()
 {
 	std::cout << "Hello uckleklsvnksie" << std::endl;
 	std::cout << "Test stemmer: mice, electrical and hopeful have nothing in common (I guess)" << std::endl;
+	std::cout << "The good, the bad and the ugly" << std::endl;
 	return 0;
 }
