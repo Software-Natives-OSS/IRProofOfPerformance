22ea1eb11a0abc3b746999f40dc90faafcd9943a
Raphael Zulliger <zulliger@software-natives.ch>
2019-06-10 07:59:05 +0200

    Adding test for stemmer

--- test.cpp
+++ test.cpp
@@ -3,5 +3,6 @@
 int main()
 {
 	std::cout << "Hello uckleklsvnksie" << std::endl;
+	std::cout << "Test stemmer: mice, eclectrical and hopeful have nothing in common (I guess)" << std::endl;
 	return 0;
 }
