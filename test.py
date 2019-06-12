afacc6aab83c3815687fc87e5ebc72348a9eee18
Raphael Zulliger <zulliger@software-natives.ch>
2019-06-12 05:52:56 +0200

    Adding a .py file that actually contains C++ to see how GitHub's language filter handles this

---
+++ test.py
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello uckleklsvnksie" << std::endl;
+	std::cout << "Test stemmer: mice, electrical and hopeful have nothing in common (I guess)" << std::endl;
+	std::cout << "The good, the bad and the ugly" << std::endl;
+	return 0;
+}
